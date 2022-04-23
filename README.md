27db29b3050387cc6a5106f9758960f42eb92124
Kaylie Sampson <kksamps2@gmail.com>
2022-04-23 01:15:32 -0400

    edited readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Hi Graham ;)
-
+# Kaylie Sampson Personal Website
+## Author: Kaylie Sampson
+## Created: 5/23/22
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Graham :)
+          Kaylie Sampson's Personal Website
         </p>
       </header>
     </div>
