ad4359943ff6449a935891c42a88d968d68447c9
Kaylie Sampson <kksamps2@gmail.com>
2022-06-29 08:55:47 -0400

    added resume link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link } from "react-scroll";
+import pdf from "../../assets/Kaylie_Sampson.pdf"
 
 function Header() {
   return (
@@ -11,9 +12,16 @@ function Header() {
       <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} >
         <div className="HeaderItem"> About </div>
       </Link>
-      <Link activeClass="active" to="whyDali" spy={true} smooth={true} offset={-70} duration={500} >
-        <div className="HeaderItem"> Why DALI? </div>
+      <Link activeClass="active" to="experience" spy={true} smooth={true} offset={-70} duration={500} >
+        <div className="HeaderItem"> Experience </div>
       </Link>
+      <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500} >
+        <div className="HeaderItem"> Projects </div>
+      </Link>
+      <a href={pdf} target="_blank" rel="noreferrer" className='HeaderItem'>Resume</a>
+      {/* <Link activeClass="active" to="whyDali" spy={true} smooth={true} offset={-70} duration={500} >
+        <div className="HeaderItem"> Resume </div>
+      </Link> */}
     </div>
   )
 }
