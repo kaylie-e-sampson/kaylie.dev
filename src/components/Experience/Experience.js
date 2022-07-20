6405286c9e614494b3579d2cceb82ea33f5d03b8
Kaylie Sampson <kksamps2@gmail.com>
2022-07-20 13:11:39 -0400

    fized some styling

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,59 +1,60 @@
 import React from 'react';
 import { Chrono } from 'react-chrono'
+import './Experience.css';
 
 const Experience = () => {
-    const items = [{
-      title: "May 1940",
-      cardTitle: "Pegasystems",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+  const items = [{
+    title: "May 1940",
+    cardTitle: "Pegasystems",
+    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+    cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+  },
+  {
+    title: "May 1940",
+    cardTitle: "Dunkirk",
+    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+    cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "http://someurl/image.jpg"
+      }
+    }
     },
     {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        url: "http://www.history.com",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        media: {
-          type: "IMAGE",
-          source: {
-            url: "http://someurl/image.jpg"
-          }
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg"
         }
-      },
-      {
-          title: "May 1940",
-          cardTitle: "Dunkirk",
-          url: "http://www.history.com",
-          cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-          cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-          media: {
-            type: "IMAGE",
-            source: {
-              url: "http://someurl/image.jpg"
-            }
-          }
-        }];
+      }
+      }];
 
     const theme = {
-        primary: '#F88080',
-        secondary: '#54677D',
-        cardBgColor: '#F6F39D',
-        titleColor: '#F88080',
-        titleCoverActive: '#F88080'
+      primary: '#F88080',
+      secondary: '#54677D',
+      cardBgColor: '#F6F39D',
+      cardForeColor: '#54677D',
+      titleColor: '#F88080',
+      titleCoverActive: '#54677D'
     }
 
     const fontSizes = {
-        cardSubtitle: '0.85rem',
-        cardText: '0.8rem',
-        cardTitle: '5rem',
-        title: '1rem',
+      cardSubtitle: '0.9rem',
+      cardText: '0.8rem',
+      cardTitle: '2rem',
+      title: '1rem',
     }
 
     return (
-        <div id ='experience' style={{  height: '1500px', paddingRight:60 }}>
-            <Chrono items={items} slideShow mode="VERTICAL" theme={theme} fontSizes={fontSizes} hideControls={true} lineWidth={5} activeItemIndex={-1} />
-        </div>
+      <div  id ='experience' style={{  height: '1500px', paddingRight:60, }}>
+          <Chrono className="experiences" items={items} slideShow mode="VERTICAL" theme={theme} fontSizes={fontSizes} hideControls={true} lineWidth={5} activeItemIndex={-1} />
+      </div>
     )
   }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,19 +6,19 @@ import pdf from "../../assets/Kaylie_Sampson.pdf"
 function Header() {
   return (
     <div className="Header">
-      <Link activeClass="active" to="title" spy={true} smooth={true} offset={-70} duration={500} >
+      <Link activeClass="active" to="title" spy={true} smooth={true} offset={-70} duration={500} style={{color: 'FFFFFF'}}>
         <div className="HeaderItem"> Home </div>
       </Link>
-      <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} >
+      <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} style={{color: 'FFFFFF'}}>
         <div className="HeaderItem"> About </div>
       </Link>
-      <Link activeClass="active" to="experience" spy={true} smooth={true} offset={-70} duration={500} >
+      <Link activeClass="active" to="experience" spy={true} smooth={true} offset={-70} duration={500} style={{color: 'FFFFFF'}}>
         <div className="HeaderItem"> Experience </div>
       </Link>
-      <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500} >
+      <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500} style={{color: 'FFFFFF'}}>
         <div className="HeaderItem"> Projects </div>
       </Link>
-      <a href={pdf} target="_blank" rel="noreferrer" className='HeaderItem'>Resume</a>
+      <a href={pdf} target="_blank" rel="noreferrer" className='HeaderItem' style={{color: 'white'}}>Resume</a>
       {/* <Link activeClass="active" to="whyDali" spy={true} smooth={true} offset={-70} duration={500} >
         <div className="HeaderItem"> Resume </div>
       </Link> */}
